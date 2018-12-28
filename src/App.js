74f6d513fadcab379f408b2b24ed7876c0c3a2ea
Oliver Mansfield <omansfield88@gmail.com>
2018-12-28 17:15:00 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Message from './components/Message'
 
 class App extends Component {
+
+  state = {
+    message: {
+      text: 'This is the messags',
+      display: false
+    },
+    latitude: 53.391501,
+    longitude: -2.9520177
+
+  }
+
+  // handleButtonPress = () => {
+  //   this.setState({
+  //     display: true
+  //   })
+  // }
+
+  componentDidMount() {
+
+     navigator.geolocation.watchPosition(
+       (position) => {
+         console.log(position.coords.latitude)
+         this.setState({
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude,
+           error: null,
+         });
+       },
+       (error) => {console.log(error)},
+       { enableHighAccuracy: true, timeout: 20000, maximumAge: 10000 },
+     );
+
+
+   }
+
+
+
+
+
   render() {
+
+
+
+    let message;
+    // if (this.state.display) {
+    //   message = <Message />
+    // }
+
+    if (this.state.latitude > 53.3915) {
+      message = <Message text={this.state.message.text}/>
+    }
+
+
+
+
+    // console.log((""+this.state.location.lat).split('').map(Number))
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Home page</h2>
+        <button onClick={this.handleButtonPress}>Show message</button>
+        <p>latitude = {this.state.latitude}</p>
+        <p>longitude = {this.state.longitude}</p>
+
+        {message}
+
       </div>
     );
   }
