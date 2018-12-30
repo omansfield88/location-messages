e4b3aa29b32409bb2ecd993e618d03c1e929f5cb
Oliver Mansfield <omansfield88@gmail.com>
2018-12-30 13:49:46 +0000

    Adding 3 different room locations

--- src/App.js
+++ src/App.js
@@ -4,27 +4,49 @@ import Message from './components/Message'
 class App extends Component {
 
   state = {
-    message: {
-      text: 'This is the messags update 4211',
-      display: false
-    },
-    latitude: 53.4210999
+    messages: [
+      {
+        text: 'Desk',
+        location: {
+          latitude: 53.3915087
+        }
+      },
+      {
+        text: 'Bedroom Window',
+        location: {
+          latitude: 53.3914673
+        }
+      },
+      {
+        text: 'Kitchen Window',
+        location: {
+          latitude: 53.3916123
+        }
+      }
+    ],
+    currentLocation: {
+      latitude: 1,
+      longitude: 1
+    }
   }
 
   handleButtonPress = () => {
     this.setState({
-      latitude: 53.4211999
+      currentLocation: {
+        latitude: 53.4216999
+      }
     })
   }
 
   componentDidMount() {
-
-     navigator.geolocation.watchPosition(
+     this.watchID = navigator.geolocation.watchPosition(
        (position) => {
-         console.log(position.coords.latitude)
+         console.log('Watch position ' + position.coords.latitude)
          this.setState({
-           latitude: position.coords.latitude,
-           longitude: position.coords.longitude,
+           currentLocation: {
+             latitude: position.coords.latitude,
+             longitude: position.coords.longitude
+           },
            error: null,
          });
        },
@@ -34,31 +56,43 @@ class App extends Component {
 
    }
 
+   componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
 
 
 
 
   render() {
 
-    console.log(this.state.latitude)
+    console.log("current lat" + this.state.currentLocation.latitude)
 
+
+    let latitudeArray = ((""+this.state.currentLocation.latitude).split('').map(Number))
+    console.log(latitudeArray);
     let message;
-    // if (this.state.display) {
-    //   message = <Message />
-    // }
 
-    if (this.state.latitude < 53.3915) {
-      message = <Message text={this.state.message.text}/>
+
+    if (latitudeArray[6] === 4) {
+      message = <Message text={this.state.messages[1].text}/>
+    }
+    else if (latitudeArray[6] === 6) {
+      message = <Message text={this.state.messages[2].text}/>
+    }
+    else if (latitudeArray[6] === 5) {
+      message = <Message text={this.state.messages[0].text}/>
     }
 
-    // console.log((""+this.state.location.lat).split('').map(Number))
+
 
     return (
       <div className="App">
         <h2>Home page</h2>
         <button onClick={this.handleButtonPress}>Show message</button>
-        <p>latitude = {this.state.latitude}</p>
-        <p>longitude = {this.state.longitude}</p>
+        <p>latitude = {this.state.currentLocation.latitude}</p>
+        <p>longitude = {this.state.currentLocation.longitude}</p>
+        <p>lat array = {latitudeArray[6]}</p>
 
         {message}
 
